94a926fbb4446fa2f98a2d2d4491b0c17b0adf87
CoSat2023 <john.mcbride2768@gmail.com>
2025-05-28 14:59:37 -0700

    initial commmit

---
+++ Maui.MathGame.CoSat2023/MathGame.Maui/Data/GameRepository.cs
@@ -0,0 +1,45 @@
+﻿using MathGame.Maui.Models;
+using SQLite;
+
+namespace MathGame.Maui.Data
+{
+    public class GameRepository
+    {
+        string _dbPath;
+        private SQLiteConnection conn;
+        private bool _initialized = false;
+
+        public GameRepository(string dbPath)
+        {
+            _dbPath = dbPath;
+        }
+
+        public void Init()
+        {
+            if (_initialized)
+                return;
+
+            conn = new SQLiteConnection(_dbPath);
+            conn.CreateTable<Game>();
+            _initialized = true;
+        }
+
+        public List<Game> GetAllGames()
+        {
+            Init();
+            return conn.Table<Game>().ToList();
+        }
+
+        public void Add(Game game)
+        {
+            conn = new SQLiteConnection(_dbPath);
+            conn.Insert(game);
+        }
+
+        public void Delete(int id)
+        {
+            conn = new SQLiteConnection(_dbPath);
+            conn.Delete(new Game { Id = id });
+        }
+    }
+}
---
+++ Maui.MathGame.CoSat2023/MathGame.Maui/PreviousGames.xaml.cs
@@ -0,0 +1,23 @@
+using MathGame.Maui.Models;
+using System;
+
+namespace MathGame.Maui;
+
+public partial class PreviousGames : ContentPage
+{
+	public PreviousGames()
+	{
+        InitializeComponent();
+		App.GameRepository.GetAllGames();
+        gamesList.ItemsSource = App.GameRepository.GetAllGames();
+	}
+
+	private void OnDelete(object sender, EventArgs e)
+	{
+		ImageButton button = (ImageButton)sender;
+
+		App.GameRepository.Delete((int)button.BindingContext);
+
+		gamesList.ItemsSource = App.GameRepository.GetAllGames();
+	}
+}
